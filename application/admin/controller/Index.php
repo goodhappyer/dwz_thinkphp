fbc8c6f1e726a0bb6537c14dcb60a45eeef894ab
su <goodhappyer@qq.com>
2016-05-02 12:06:50 +0000

    基类CACHE配置OK

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -1,8 +1,17 @@
 <?php
 namespace app\admin\controller;
-use app\common\controller\test;
+use app\common\controller\Base;
+use think\Cache;
 
-class Index extends test
+class Index extends Base
 {
-	
+	protected function _initialize() 
+	{
+		parent::_initialize();
+	}
+	public function index()
+	{
+		dump(Cache::get("a"));	
+	}
 }
+
---
+++ application/common/controller/Base.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * 控制器基类
+ */
+namespace app\common\controller;
+use think\Controller;
+use think\Db;
+use think\Cache;
+
+class Base extends Controller
+{
+	protected function _initialize() 
+	{
+		/*初始化配置*/
+		foreach(Db::name('setting')->field("name,data")->select() as $v)
+		{
+			Cache::set($v['name'],$v['data']);
+		}
+	}
+	public function index()
+	{
+		echo "test";
+	}
+	/**
+	 * 加密
+	 *
+	 * @param unknown $code        	
+	 * @return string
+	 */
+	protected function encrypt($code) {
+		return base64_encode ( mcrypt_encrypt ( MCRYPT_RIJNDAEL_256, md5 ( "" ), $code, MCRYPT_MODE_ECB, mcrypt_create_iv ( mcrypt_get_iv_size ( MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB ), MCRYPT_RAND ) ) );
+	}
+	/**
+	 * 解密
+	 *
+	 * @param unknown $code        	
+	 * @return string
+	 */
+	protected function decrypt($code) {
+		return mcrypt_decrypt ( MCRYPT_RIJNDAEL_256, md5 ( "" ), base64_decode ( $code ), MCRYPT_MODE_ECB, mcrypt_create_iv ( mcrypt_get_iv_size ( MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB ), MCRYPT_RAND ) );
+	}
+
+}
