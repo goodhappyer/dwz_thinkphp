9d5ad24969c1503e9a11606e311967e77aa5d4cb
goodhappyer <goodhappyer@qq.com>
2016-05-20 18:07:40 +0800

    jqgrid的php控制类重写，

--- application/admin/controller/AddTrait.php
+++ application/admin/controller/AddTrait.php
@@ -1,22 +1,23 @@
-<?php 
+<?php
 namespace app\admin\controller;
 
 trait addTrait
 {
-	public function add()
 
+	public function add ()
+	
 	{
-		if(IS_POST)
+		if (IS_POST)
 		{
 			$fields=$this->_mod::db()->getTableInfo("","fields");
-			foreach($fields as $v)
+			foreach ($fields as $v)
 			{
-				if(INPUT::request($v)!=NULL)
+				if (INPUT::request($v) != NULL)
 				{
-					$this->_mod->$v=INPUT::request($v);
-				}	
+					$this->_mod->$v = INPUT::request($v);
+				}
 			}
-			if($this->_mod->save())
+			if ($this->_mod->save())
 			{
 				$this->dwz_ajax_return_ok();
 			}
--- application/common/model/JQGrid.php
+++ application/common/model/JQGrid.php
@@ -1,93 +1,123 @@
 <?php
-
 namespace app\common\model;
-
 use PDO;
 use think\Input;
 use think\Db;
-
-class JQGrid {
-	var $jqgrid_talbe = "jqgrid_info";
-	var $table_name;
+class JQGrid_item
+{
+	private $jqgrid_talbe = "jqgrid_info";
 	var $debug = true;
-	var $table_id;
-	var $pager_id;
-	var $js_init_function = "pageInit";
-	var $js_jqg_datatype = "local";
-	var $js_jqg_height = "250";
-	var $js_jqg_colNames = array ();
-	var $js_jqg_colModel = array ();
-	var $js_jqg_multiselect = true;
-	var $js_jqg_caption;
-	var $js_jqg_data = array ();
-	function __construct($items = []) {
-		foreach ( $items as $k => $v ) {
-			$this->$k = $v;
+	// 存放jqgrid信息的表
+	var $data_table;
+	// js的初始化函数
+	var $datatype;
+	var $height;
+	var $colNames;
+	var $colModel;
+	var $multiselect;
+	var $caption;
+	function create_js_item ()
+	{
+		if (empty($this->data_table))
+		{
+			throw new Exception("data_table is empty");
 		}
-		if ($this->debug) {
-			// 開發時開啓debug
-			$tableinfo = Db::name ( $this->table_name )->getTableInfo ();
-			foreach ( $tableinfo ['fields'] as $k => $v ) {
-				if (! Db::name ( $this->jqgrid_talbe )->where ( "table_name", $this->table_name )->where ( "field_name", $v )->find ()) {
-					$d ['table_name'] = $this->table_name;
-					$d ['field_name'] = $v;
-					$d ['jqgrid_name'] = $v;
-					$d ['jqgrid_index'] = $v;
-					
-					if ($tableinfo ['bind'] [$v] == PDO::PARAM_STR) {
-						$d ['jqgrid_width'] = 50;
-						$d ['jqgrid_align'] = 'right';
-						$d ['jqgrid_sorttype'] = false;
-					} else if ($tableinfo ['bind'] [$v] == PDO::PARAM_INT) {
-						$d ['jqgrid_width'] = 11;
-						$d ['jqgrid_align'] = 'left';
-						$d ['jqgrid_sorttype'] = 'int';
-					} else {
-						$d ['jqgrid_width'] = 5;
-						$d ['jqgrid_align'] = 'center';
-						$d ['jqgrid_sorttype'] = false;
+		if ($this->debug)
+		{
+			// 開發時開啓debug,检查表中是否增加过字段
+			$tableinfo = Db::name($this->data_table)->getTableInfo();
+			foreach ($tableinfo['fields'] as $k => $v)
+			{
+				if (! Db::name($this->jqgrid_talbe)->where("data_table",$this->data_table)
+					->where("field_name",$v)
+					->find())
+				{
+					$d['table_name'] = $this->table_name;
+					$d['field_name'] = $v;
+					$d['jqgrid_name'] = $v;
+					$d['jqgrid_index'] = $v;
+					if ($tableinfo['bind'][$v] == PDO::PARAM_STR)
+					{
+						$d['jqgrid_width'] = 50;
+						$d['jqgrid_align'] = 'right';
+						$d['jqgrid_sorttype'] = false;
 					}
-					Db::name ( $this->jqgrid_talbe )->insert ( $d );
+					else if ($tableinfo['bind'][$v] == PDO::PARAM_INT)
+					{
+						$d['jqgrid_width'] = 11;
+						$d['jqgrid_align'] = 'left';
+						$d['jqgrid_sorttype'] = 'int';
+					}
+					else
+					{
+						$d['jqgrid_width'] = 5;
+						$d['jqgrid_align'] = 'center';
+						$d['jqgrid_sorttype'] = false;
+					}
+					Db::name($this->jqgrid_talbe)->insert($d);
 				}
 			}
 		}
-	}
-	function show() {
-		if (empty ( $this->table_name )) {
-			throw new Exception ( "table_name is empty" );
-		}
-		if (empty ( $this->table_id )) {
-			$this->table_id = "list_" . $this->table_name;
-		}
-		if (empty ( $this->pager_id )) {
-			$this->table_id = "pager_" . $this->table_name;
-		}	
-		$cols=Db::name ( $this->jqgrid_talbe )->where ( "table_name", $this->table_name )->select ();
-		$colNames=[];
-		$colMode=[];
-		foreach($clos as $v)	
+		
+
+		if (empty($this->table_id))
 		{
-			$colNames[]=$v['jqgrid_name'];	
-			$col=[];
-			$col['name']=$v['field_name'];
-			$col['index']=$v['jqgrid_index'];
-			$col['width']=$v['jqgrid_width'];
-			$col['align']=$v['jqgrid_align'];
-			$col['sorttype']=$v['jqgrid_sorttype'];
-			$colMode[]=$col;
+			$this->table_id = "list_" . $this->data_table;
 		}
-		$d=array();
-		$d['datatype']=$this->js_jqg_datatype;
-		$d['height']=$this->js_jqg_height;
-		$d['colNames']=$this->js_jqg_colNames;
-		$d['colModel']=$this->js_jqg_colModel;
-		$d['multiselect']=$this->js_jqg_multiselect;
-		$d['caption']=$this->js_jqg_caption;
-		echo '$(function(){'.$this->js_init_function.'();});';
-		echo 'function '.$this->js_init_function.'(){';
-			echo 'jQuery("#'.$this->table_id.'").jqGrid(';
-				echo json_encode($d);
-			echo ')';
+		if (empty($this->pager_id))
+		{
+			$this->table_id = "pager_" . $this->data_table;
+		}
+		$cols = Db::name($this->jqgrid_talbe)->where("data_table",$this->data_table)->select();
+		foreach ($clos as $v)
+		{
+			$this->colNames[] = $v['jqgrid_name'];
+			$col = [ ];
+			$col['name'] = $v['field_name'];
+			$col['index'] = $v['jqgrid_index'];
+			$col['width'] = $v['jqgrid_width'];
+			$col['align'] = $v['jqgrid_align'];
+			$col['sorttype'] = $v['jqgrid_sorttype'];
+			$this->colModel[] = $col;
+		}
+	}
+	function create_data()
+	{
+		
+	}
+}
+class JQGrid
+{
+	// 用table表生成jqgrid
+	var $table_id;
+	// 把内容放在$table_id中
+	var $pager_id;
+	// 把分页放在pager_id中
+	var $js_init_function = "pageInit";
+	var $jqgrid_item; // jqgrid的配置项
+	function __construct ($items)
+	{
+		if(is_string($items))
+		{
+			$items['data_table']=$items;
+		}
+		$jqgrid_item = new JQGrid_item();
+		foreach ($items as $k => $v)
+		{
+			if (property_exists($jqgrid_item,$k))
+			{
+				$jqgrid_item->$k = $v;
+			}
+			$this->$k = $v;
+		}
+	}
+	function show ()
+	{
+		echo '$(function(){' . $this->js_init_function . '();});';
+		echo 'function ' . $this->js_init_function . '(){';
+		echo 'jQuery("#' . $this->table_id . '").jqGrid(';
+		echo json_encode($d);
+		echo ')';
 		echo '}';
 	}
 }
